5958bf68db6493a6a2aa66bc5a15f9179c2aeb63
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2026-07-20 10:53:00 +0200

    Supplier

--- src/j8/lambdas/my_code/FI_from_API.java
+++ src/j8/lambdas/my_code/FI_from_API.java
@@ -1,13 +1,17 @@
 package j8.lambdas.my_code;
 
+import java.time.LocalTime;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class FI_from_API {
 
     static void main() {
         FI_from_API fiAPI = new FI_from_API();
-        fiAPI.predicate();
+//        fiAPI.predicate();
+        fiAPI.supplier();
+
     }
 
     public void predicate() {
@@ -16,6 +20,17 @@ public class FI_from_API {
 
         BiPredicate<String, Integer> checkLength = (str, len) -> str.length() == len;
         System.out.println(checkLength.test("Marco", 5));
+    }
+
+    public void supplier() {
+        Supplier<StringBuilder> supSB = () -> new StringBuilder();
+        System.out.println("Supplier SB: " + supSB.get().append("SK"));
+
+        Supplier<LocalTime> supTime = () -> LocalTime.now();
+        System.out.println("Supplier time: " + supTime.get());
+
+        Supplier<Double> sRandom = () -> Math.random();
+        System.out.println(sRandom.get());
     }
 
 }
