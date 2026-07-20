8f5b84b575c838ed5486e2af9b13fc684255b7d7
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2026-07-20 11:19:42 +0200

    Consumer and BiConsumer

--- src/j8/lambdas/my_code/FI_from_API.java
+++ src/j8/lambdas/my_code/FI_from_API.java
@@ -1,16 +1,18 @@
 package j8.lambdas.my_code;
 
 import java.time.LocalTime;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.*;
 
 public class FI_from_API {
 
     static void main() {
         FI_from_API fiAPI = new FI_from_API();
 //        fiAPI.predicate();
-        fiAPI.supplier();
+//        fiAPI.supplier();
+        fiAPI.consumer();
 
     }
 
@@ -31,6 +33,26 @@ public class FI_from_API {
 
         Supplier<Double> sRandom = () -> Math.random();
         System.out.println(sRandom.get());
+    }
+
+    public void consumer() {
+        Consumer<String> printC = s -> System.out.println(s);
+        printC.accept("To be or not to be, that is the question");
+
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Mary");
+        names.forEach(printC);
+
+        var mapCapitalCities = new HashMap<String, String>();
+
+        BiConsumer<String, String> biCon = (key,value) -> mapCapitalCities.put(key, value);
+        biCon.accept("Dublin", "Ireland");
+        biCon.accept("Washington D.C.", "USA");
+        System.out.println(mapCapitalCities);
+
+        BiConsumer<String, String> mapPrint = (key, value) -> System.out.println(key + ": " + value);
+        mapCapitalCities.forEach(mapPrint);
     }
 
 }
