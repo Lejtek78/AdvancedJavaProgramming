c8798a264374693a4efda44867dcb484c3d7a856
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2026-07-24 15:13:57 +0200

    Select One

--- src/j8/jdbc/my_code/BankService.java
+++ src/j8/jdbc/my_code/BankService.java
@@ -1,8 +1,6 @@
 package j8.jdbc.my_code;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class BankService {
 
@@ -11,6 +9,44 @@ public class BankService {
 
     static void main() {
         bank = new BankService();
+
+        bank.retrieveOne();
+        System.out.println();
+//        bank.retrieveAll();
+
+    }
+
+    private void retrieveOne() {
+        System.out.println(bank.getAccountDetails("123456", "12345678"));
+    }
+
+    private BankAccount getAccountDetails(String branchCode, String accountNumber) {
+        String selectSQL = "SELECT * FROM bank_db.bank_table WHERE (branch_code = ? AND account_number = ?)";
+        BankAccount bankAccount = null;
+
+        try (PreparedStatement ps = con.prepareStatement(selectSQL)) {
+            ps.setString(1, branchCode);
+            ps.setString(2, accountNumber);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (!rs.next()) {
+                return bankAccount;
+            }
+
+            bankAccount = new BankAccount(
+                    rs.getString("branch_code"),
+                    rs.getString(2),
+                    rs.getString("cust_name"),
+                    rs.getString("cust_address"),
+                    rs.getDouble("balance"));
+
+        } catch (SQLException sqle) {
+            System.err.println("SqlException in getAccountDetails()");
+            sqle.printStackTrace();
+        }
+
+        return bankAccount;
     }
 
     public BankService() {
